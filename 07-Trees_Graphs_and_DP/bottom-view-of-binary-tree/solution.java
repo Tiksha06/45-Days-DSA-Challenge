1494185e2b9736b8b978ac7f9732b4699955aeec
Tiksha06 <tikshanagpal11940@gmail.com>
2026-07-04 23:50:00 +0530

    Day 34 wrap up 🚀

---
+++ 07-Trees_Graphs_and_DP/bottom-view-of-binary-tree/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    static class Pair {
+        Node node;
+        int hd;
+        
+        Pair(Node node, int hd) {
+            this.node=node;
+            this.hd=hd;
+        }
+    }
+    public ArrayList<Integer> bottomView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root==null) return ans;
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair> q=new LinkedList<>();
+        
+        q.add(new Pair(root,0));
+        
+        while(!q.isEmpty()){
+            Pair curr=q.remove();
+            Node node=curr.node;
+            int hd=curr.hd;
+            
+            map.put(hd,node.data);
+            
+            if(node.left!=null){
+                q.add(new Pair(node.left,hd-1));
+            }
+            if(node.right != null){
+                q.add(new Pair(node.right,hd+1));
+            }
+        }
+        
+        for(int value:map.values()){
+            ans.add(value);
+        }
+        
+        return ans;
+    }
+}
